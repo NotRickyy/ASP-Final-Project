0dd2dced639a12a6d0a6dbfaa0cb2738bda68543
Ricky Au <rickyau.9@gmail.com>
2016-03-27 15:34:25 -0400

    Starting pages for ASP

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASP_Final_Project.Startup))]
+namespace ASP_Final_Project
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
